445f95cf1c1481f7bd752de09d71de66839c97d7
buntyhim <bunty.him@live.com>
2022-04-28 13:04:17 +0530

    First Commit

---
+++ m4/GCC_Project/output/depend/LCD16x2.d
@@ -0,0 +1,50 @@
+/home/hy/Projects/ee5811/arm2lcd/m4/GCC_Project/output/LCD16x2.o: \
+ /home/hy/Projects/ee5811/arm2lcd/m4/src/LCD16x2.c \
+ /home/hy/qorc-sdk/HAL/inc/eoss3_hal_timer.h \
+ /home/hy/qorc-sdk/Libraries/Utils/inc/common.h \
+ /home/hy/qorc-sdk/HAL/inc/test_types.h \
+ /home/hy/qorc-sdk/HAL/inc/eoss3_hal_def.h \
+ /home/hy/Projects/ee5811/arm2lcd/m4/inc/LCD16x2.h \
+ /home/hy/Projects/ee5811/arm2lcd/m4/inc/Fw_global_config.h \
+ /home/hy/qorc-sdk/Libraries/Utils/inc/dbg_uart.h \
+ /home/hy/qorc-sdk/FreeRTOS/include/FreeRTOS.h \
+ /home/hy/Projects/ee5811/arm2lcd/m4/inc/FreeRTOSConfig.h \
+ /home/hy/qorc-sdk/FreeRTOS/include/projdefs.h \
+ /home/hy/qorc-sdk/FreeRTOS/include/portable.h \
+ /home/hy/qorc-sdk/FreeRTOS/include/deprecated_definitions.h \
+ /home/hy/qorc-sdk/FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h \
+ /home/hy/qorc-sdk/FreeRTOS/include/mpu_wrappers.h \
+ /home/hy/Projects/ee5811/arm2lcd/m4/inc/LCD16x2_io.h \
+ /home/hy/Projects/ee5811/arm2lcd/m4/inc/PyHal_GPIO.h
+
+/home/hy/qorc-sdk/HAL/inc/eoss3_hal_timer.h:
+
+/home/hy/qorc-sdk/Libraries/Utils/inc/common.h:
+
+/home/hy/qorc-sdk/HAL/inc/test_types.h:
+
+/home/hy/qorc-sdk/HAL/inc/eoss3_hal_def.h:
+
+/home/hy/Projects/ee5811/arm2lcd/m4/inc/LCD16x2.h:
+
+/home/hy/Projects/ee5811/arm2lcd/m4/inc/Fw_global_config.h:
+
+/home/hy/qorc-sdk/Libraries/Utils/inc/dbg_uart.h:
+
+/home/hy/qorc-sdk/FreeRTOS/include/FreeRTOS.h:
+
+/home/hy/Projects/ee5811/arm2lcd/m4/inc/FreeRTOSConfig.h:
+
+/home/hy/qorc-sdk/FreeRTOS/include/projdefs.h:
+
+/home/hy/qorc-sdk/FreeRTOS/include/portable.h:
+
+/home/hy/qorc-sdk/FreeRTOS/include/deprecated_definitions.h:
+
+/home/hy/qorc-sdk/FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h:
+
+/home/hy/qorc-sdk/FreeRTOS/include/mpu_wrappers.h:
+
+/home/hy/Projects/ee5811/arm2lcd/m4/inc/LCD16x2_io.h:
+
+/home/hy/Projects/ee5811/arm2lcd/m4/inc/PyHal_GPIO.h:
